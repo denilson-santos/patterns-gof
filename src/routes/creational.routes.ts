37c64f700297464d9246edfc28a3d5a0f7494cff
Denilson <denilson.silvasantos.contato@gmail.com>
2022-08-30 22:48:47 -0300

    feat: create module singleton pattern example

--- src/routes/creational.routes.ts
+++ src/routes/creational.routes.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 
 import { MyDatabaseClassic } from '@creational/singleton/db/my-database-classic';
+import { MyDatabaseModule } from '@creational/singleton/db/my-database-module';
 
 export const creationalRouter = Router();
 
@@ -32,8 +33,47 @@ creationalRouter.get('/singleton', (request: Request, response: Response) => {
   const users2 = obj2.getUsers();
 
   response.json({
+    type: 'classic',
     users,
     users2,
     result: users === users2,
   });
 });
+
+creationalRouter.get(
+  '/singleton-module',
+  (request: Request, response: Response) => {
+    const obj1 = MyDatabaseModule;
+    const obj2 = MyDatabaseModule;
+
+    obj1.setUsers([
+      {
+        name: 'Denilson',
+        email: 'denilson@gmail.com',
+        age: 23,
+      },
+      {
+        name: 'João',
+        email: 'joao@gmail.com',
+        age: 33,
+      },
+    ]);
+
+    const users = obj1.getUsers();
+
+    obj2.setUser({
+      name: 'Márcia',
+      email: 'marcia@gmail.com',
+      age: 22,
+    });
+
+    const users2 = obj2.getUsers();
+
+    response.json({
+      type: 'module',
+      users,
+      users2,
+      result: users === users2,
+    });
+  }
+);
